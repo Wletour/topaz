8eab8366e88d547019fa723021648333b8b84234
Illzbee <65986311+illzbee@users.noreply.github.com>
2020-08-20 21:19:21 -0400

    Update blood_rage.lua

--- scripts/globals/effects/blood_rage.lua
+++ scripts/globals/effects/blood_rage.lua
@@ -5,10 +5,12 @@
 -----------------------------------
 
 function onEffectGain(target, effect)
+target:addMod(tpz.mod.CRITHITRATE, 20)
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
+target:addMod(tpz.mod.CRITHITRATE, -20)
 end
