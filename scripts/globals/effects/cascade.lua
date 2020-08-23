80d05eb587d7472c2f6301efae96ce4d71beb6a2
Illzbee <65986311+illzbee@users.noreply.github.com>
2020-08-22 21:25:11 -0400

    Add BLM abilities

--- scripts/globals/effects/cascade.lua
+++ scripts/globals/effects/cascade.lua
@@ -1,16 +1,16 @@
 -----------------------------------
 --
---     tpz.effect.MANA_WALL
---
+--     tpz.effect.CASCADE
+--     Grants a damage bonus to the next elemental magic spell cast based on TP consumed.
 -----------------------------------
 
 function onEffectGain(target, effect)
-target:addMod(tpz.mod.DMG, 50)
+target:addMod(tpz.mod.MATT, 100)
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-target:delMod(tpz.mod.DMG, 50)
+target:delMod(tpz.mod.MATT)
 end
