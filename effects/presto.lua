ee92875ed35546469106201f9b2d7b8ddf00d6b7
Illzbee <65986311+illzbee@users.noreply.github.com>
2020-08-23 22:25:19 -0400

    Add DNC abilities

---
+++ effects/presto.lua
@@ -0,0 +1,14 @@
+-----------------------------------
+--
+--     tpz.effect.PRESTO
+--     
+-----------------------------------
+
+function onEffectGain(target, effect)
+end
+
+function onEffectTick(target, effect)
+end
+
+function onEffectLose(target, effect)
+end
