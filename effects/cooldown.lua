b195cc19cfd5132b3a476f18dcb076170d57692c
Illzbee <65986311+illzbee@users.noreply.github.com>
2020-08-23 22:11:06 -0400

    Add PUP

---
+++ effects/cooldown.lua
@@ -0,0 +1,14 @@
+-----------------------------------
+--
+--     tpz.effect.COOLDOWN
+--     
+-----------------------------------
+
+function onEffectGain(target, effect)
+end
+
+function onEffectTick(target, effect)
+end
+
+function onEffectLose(target, effect)
+end
---
+++ effects/tactical_switch.lua
@@ -0,0 +1,14 @@
+-----------------------------------
+--
+--     tpz.effect.TACTICAL_SWITCH
+--     
+-----------------------------------
+
+function onEffectGain(target, effect)
+end
+
+function onEffectTick(target, effect)
+end
+
+function onEffectLose(target, effect)
+end
