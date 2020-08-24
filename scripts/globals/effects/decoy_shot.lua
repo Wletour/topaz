102b0cbf67120e6ebae17b1bf5cb79050924d851
Illzbee <65986311+illzbee@users.noreply.github.com>
2020-08-23 21:36:32 -0400

    Add RNG

---
+++ scripts/globals/effects/decoy_shot.lua
@@ -0,0 +1,14 @@
+-----------------------------------
+--
+--     tpz.effect.DECOY_SHOT
+--     
+-----------------------------------
+
+function onEffectGain(target, effect)
+end
+
+function onEffectTick(target, effect)
+end
+
+function onEffectLose(target, effect)
+end
