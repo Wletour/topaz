02d37a4fd6056d1943eca45f132fdad07286cdea
Illzbee <65986311+illzbee@users.noreply.github.com>
2020-08-24 21:40:45 -0400

    added missing job abilities and effect
    pet abilities still need completion

---
+++ scripts/globals/abilities/presto.lua
@@ -0,0 +1,18 @@
+-----------------------------------
+-- Ability: Presto
+-- Description Enhances the effect of your next step and grants you an additional finishing move.
+-- Obtained: DNC Level 77
+-- Recast Time: 00:00:15 (Step)
+-- Duration: 00:00:30
+-----------------------------------
+require("scripts/globals/settings")
+require("scripts/globals/status")
+-----------------------------------
+
+function onAbilityCheck(player,target,ability)
+    return 0,0
+end
+
+function onUseAbility(player,target,ability)
+    target:addStatusEffect(tpz.effect.PRESTO,19,1,30)
+end
