647ab0e7b3e04772b818ff6ea330d2b9c9d96704
Illzbee <65986311+illzbee@users.noreply.github.com>
2020-08-24 21:37:51 -0400

    Add files via upload

---
+++ scripts/globals/effects/brazen_strength.lua
@@ -0,0 +1,17 @@
+-----------------------------------
+--
+--     tpz.effect.BRAZEN_STRENGTH
+--
+-----------------------------------
+
+function onEffectGain(target, effect)
+target:addMod(tpz.mod.DOUBLE_ATTACK, 100)
+end
+
+function onEffectTick(target, effect)
+target:addMod(tpz.mod.DOUBLE_ATTACK, -25)
+end
+
+function onEffectLose(target, effect)
+target:addMod(tpz.mod.DOUBLE_ATTACK, -50)
+end
--- scripts/globals/effects/larceny.lua
+++ scripts/globals/effects/larceny.lua
@@ -1,14 +1,18 @@
 -----------------------------------
 --
---     tpz.effect.DRAGON_BREAKER
---     
+--     tpz.effect.LARCENY
+--
 -----------------------------------
 
 function onEffectGain(target, effect)
+target:addMod(tpz.mod.AUGMENTS_AURA_STEAL, 100)
+target:addMod(tpz.mod.STEAL, 100)
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
+target:delMod(tpz.mod.AUGMENTS_AURA_STEAL, 100)
+target:delMod(tpz.mod.STEAL, 100)
 end
