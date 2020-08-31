72504db7517c78c81d5f638b4fa49360959850f1
Illzbee <65986311+illzbee@users.noreply.github.com>
2020-08-30 20:09:48 -0400

    edited flourish iii's

--- scripts/globals/abilities/climactic_flourish.lua
+++ scripts/globals/abilities/climactic_flourish.lua
@@ -1,10 +1,10 @@
 -----------------------------------
--- Ability: Striking Flourish
--- Description Allows you to deliver a twofold attack. Requires at least two finishing moves.
--- Obtained: DNC Level 89
--- Recast Time: 00:00:30 (Flourishes III)
+-- Ability: Ternary Flourish
+-- Description Allows you to deliver a threefold attack. Requires at least three finishing moves.
+-- Obtained: DNC Level 93
+-- Recast Time: 00:00:45 (Flourishes III)
 -- Duration: 00:01:00
--- Cost: 2 Finishing Move charges
+-- Cost: 3 Finishing Move charges
 -----------------------------------
 require("scripts/globals/settings")
 require("scripts/globals/status")
@@ -37,20 +37,19 @@ function onUseAbility(player, target, ability)
 
     if (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_1)) then
         player:delStatusEffect(tpz.effect.FINISHING_MOVE_1)
-        player:addStatusEffect(tpz.effect.STRIKING_FLOURISH, 1, 0, 60, 0, player:getMerit(tpz.merit.STRIKING_FLOURISH_EFFECT))
+        player:addStatusEffect(tpz.effect.CLIMACTIC_FLOURISH, 1, 20, 60, 0, 0)
     elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_2)) then
         player:delStatusEffect(tpz.effect.FINISHING_MOVE_2)
-        player:addStatusEffect(tpz.effect.STRIKING_FLOURISH, 2, 0, 60, 0, player:getMerit(tpz.merit.STRIKING_FLOURISH_EFFECT))
+        player:addStatusEffect(tpz.effect.CLIMACTIC_FLOURISH, 2, 40, 60 0, 0)
     elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_3)) then
         player:delStatusEffect(tpz.effect.FINISHING_MOVE_3)
-        player:addStatusEffect(tpz.effect.STRIKING_FLOURISH, 3, 0, 60, 0, player:getMerit(tpz.merit.STRIKING_FLOURISH_EFFECT))
+        player:addStatusEffect(tpz.effect.CLIMACTIC_FLOURISH, 3, 60, 60 0, 0)
     elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_4)) then
         player:delStatusEffect(tpz.effect.FINISHING_MOVE_4)
-        player:addStatusEffect(tpz.effect.FINISHING_MOVE_1, 1, 0, 7200)
-        player:addStatusEffect(tpz.effect.STRIKING_FLOURISH, 3, 0, 60, 0, player:getMerit(tpz.merit.STRIKING_FLOURISH_EFFECT))
+        player:addStatusEffect(tpz.effect.CLIMACTIC_FLOURISH, 4, 80, 60 0, 0)
     elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_5)) then
         player:delStatusEffect(tpz.effect.FINISHING_MOVE_5)
-        player:addStatusEffect(tpz.effect.FINISHING_MOVE_2, 1, 0, 7200)
-        player:addStatusEffect(tpz.effect.STRIKING_FLOURISH, 3, 0, 60, 0, player:getMerit(tpz.merit.STRIKING_FLOURISH_EFFECT))
+        player:addStatusEffect(tpz.effect.CLIMACTIC_FLOURISH, 5, 100, 60 0, 0)
     end
+
 end
